0edb5bbcf51ac0158d677b789905e60732ce1687
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-20 14:49:56 +0200

    adding swagger

---
+++ src/main/java/com/example/SwaggerBooks/configuration/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.SwaggerBooks.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Books Rest API")
+                .description("Restful Api for Books from Google APIs")
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    public Docket bookApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .paths(PathSelectors.regex("/api.*"))
+                .build();
+    }
+
+}
