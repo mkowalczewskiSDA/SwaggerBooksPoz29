3f629972b3a131a7eac7f62b11a18e7ac51192ce
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-26 09:18:40 +0200

    properties

--- src/main/java/com/example/SwaggerBooks/util/JsonParser.java
+++ src/main/java/com/example/SwaggerBooks/util/JsonParser.java
@@ -4,6 +4,8 @@ import com.example.SwaggerBooks.model.Book;
 import com.example.SwaggerBooks.model.Item;
 import com.example.SwaggerBooks.model.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -14,15 +16,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@PropertySource("classpath:application.properties")
 public class JsonParser {
 
-    private final String url = "https://www.googleapis.com/books/v1/volumes?q=java&maxResults=40";
+    @Value("${parser.source}")
+    private String source;
+
+    //private final String url = "https://www.googleapis.com/books/v1/volumes?q=java&maxResults=40";
     private final List<Book> bookList = new ArrayList<>();
 
     @PostConstruct
     public void parseJson() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        Response response = mapper.readValue(new URL(url), Response.class);
+        Response response = mapper.readValue(new URL(source), Response.class);
         response.getItems().forEach(item -> bookList.add(new Book(item)));
         System.out.println("test");
     }
