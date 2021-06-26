b0ba7c012bfcbe80c2e6220e9b1f4bbb8330904c
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-26 11:33:54 +0200

    aspect

---
+++ src/main/java/com/example/SwaggerBooks/aspect/SimpleAspect.java
@@ -0,0 +1,9 @@
+package com.example.SwaggerBooks.aspect;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.aspectj.lang.annotation.Aspect;
+
+@EnableAspectJAutoProxy
+@Aspect
+public class SimpleAspect {
+}
