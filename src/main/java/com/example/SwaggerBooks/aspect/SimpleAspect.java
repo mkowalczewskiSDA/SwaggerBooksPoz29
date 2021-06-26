cba38c66ddc1383b9db6f59783c04000584d87eb
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-26 11:45:02 +0200

    aspect more

--- src/main/java/com/example/SwaggerBooks/aspect/SimpleAspect.java
+++ src/main/java/com/example/SwaggerBooks/aspect/SimpleAspect.java
@@ -1,9 +1,25 @@
 package com.example.SwaggerBooks.aspect;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @EnableAspectJAutoProxy
 @Aspect
+@Component
+@Slf4j
 public class SimpleAspect {
+
+    @Pointcut("execution(* com.example.SwaggerBooks..*(..))")
+    public void anyMethod(){ }
+
+    @Before("anyMethod()")
+    public void beforeAnyMethod(JoinPoint joinPoint) {
+        System.out.println(joinPoint.getSignature().getDeclaringTypeName());
+        log.info("... before any method ... "+joinPoint.getSignature().getName());
+    }
 }
